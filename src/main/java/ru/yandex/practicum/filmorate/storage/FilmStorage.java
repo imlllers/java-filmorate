de1458553bd4af6cc85c286c136e708b5d8cba22
Davli4 <vladislauskirey@yandex.by>
2026-02-18 22:20:53 +0300

    fix checkstyle: 2

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
